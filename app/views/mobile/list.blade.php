82463414553a24d4f040e86ad4915cb6858ee12c
unknown <492994082@qq.com>
2015-06-11 14:49:33 +0800

    fix

--- app/views/mobile/list.blade.php
+++ app/views/mobile/list.blade.php
@@ -83,11 +83,11 @@ var imgUrl = "{{__PUBLIC__.'/img'}}";
 var bind="{{$bind}}";
 function bindFun(){
 //    alert(bind);
-    if(bind!=200){
-        alert("亲~你还没有绑定学号，绑定后才可以点歌哦!\n关注重邮小帮手（cyxbswx），输入关键字绑定学号，即可！");
-        $(".Song").remove();
-        return false;
-    }
+//    if(bind!=200){
+//        alert("亲~你还没有绑定学号，绑定后才可以点歌哦!\n关注重邮小帮手（cyxbswx），输入关键字绑定学号，即可！");
+//        $(".Song").remove();
+//        return false;
+//    }
 }
 var status = "{{$status}}";
 if(status == 0){
