9c0a64019a690f4a2a29c6ef9550ef7ff128e347
unknown <492994082@qq.com>
2015-06-11 14:39:42 +0800

    fix

--- app/views/mobile/list.blade.php
+++ app/views/mobile/list.blade.php
@@ -84,7 +84,7 @@ var bind="{{$bind}}";
 function bindFun(){
 //    alert(bind);
     if(bind!=200){
-        alert("亲~你还没有绑定学号，绑定后才可以点歌哦!\n关注重邮小帮手（cyxbswx）,输入关键字绑定学号，即可！");
+        alert("亲~你还没有绑定学号，绑定后才可以点歌哦!\n关注重邮小帮手（cyxbswx），输入关键字绑定学号，即可！");
         $(".Song").remove();
         return false;
     }
